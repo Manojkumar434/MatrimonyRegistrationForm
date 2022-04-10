e6c7825a9f73285fc187cf899f96ed36cc047313
Manojkumar434 <manojgeetha12.10.2001@gmail.com>
2022-04-10 22:33:08 +0530

    All finish in front-end

---
+++ MatrimonyRegistrationForm/src/main/java/MatrimonyForm/MatrimonyRegistrationForm/MatrimonyRegistrationFormApplication.java
@@ -0,0 +1,13 @@
+package MatrimonyForm.MatrimonyRegistrationForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrimonyRegistrationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrimonyRegistrationFormApplication.class, args);
+	}
+
+}
---
+++ MatrimonyRegistrationForm/src/test/java/MatrimonyForm/MatrimonyRegistrationForm/MatrimonyRegistrationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package MatrimonyForm.MatrimonyRegistrationForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrimonyRegistrationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
